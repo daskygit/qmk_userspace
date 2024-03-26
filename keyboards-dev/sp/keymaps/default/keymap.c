7ef46a08f6b15419bdd6a9fa364c23e912ebe813
Dasky <32983009+daskygit@users.noreply.github.com>
2024-03-26 20:53:48 +0000

    update sp

--- keyboards-dev/sp/keymaps/default/keymap.c
+++ keyboards-dev/sp/keymaps/default/keymap.c
@@ -20,8 +20,8 @@ void keyboard_post_init_user(void) {
 
 #ifdef MULTI_POINTING_ENABLE
 const pointing_device_config_t pointing_device_configs[POINTING_DEVICE_COUNT] = {
-  {.driver = &azoteq_iqs5xx_driver_default, .config = &azoteq_iqs5xx_i2c_config_default, .side = LEFT, .throttle = 1, .motion = {.pin = TPS_RDY, .active_low = false}},
-  {.driver = &azoteq_iqs5xx_driver_default, .config = &azoteq_iqs5xx_i2c_config_default, .side = RIGHT, .throttle = 1, .rotation = ROTATE_180, .motion = {.pin = TPS_RDY, .active_low = false}},
-  {.driver = &cirque_driver_spi_default, .config = &cirque_config_spi_default, .side = LEFT, .throttle = 1, .rotation = ROTATE_270, .motion = {.pin = CIRQUE_RDY, .active_low = false}},
+    [0] = {.driver = &azoteq_iqs5xx_driver_default, .comms_config = &azoteq_iqs5xx_i2c_config_default, .device_config = &azoteq_iqs5xx_device_config_default, .side = LEFT, .throttle = 1, .motion = {.pin = TPS_RDY, .active_low = false}},
+    [1] = {.driver = &azoteq_iqs5xx_driver_default, .comms_config = &azoteq_iqs5xx_i2c_config_default, .device_config = &azoteq_iqs5xx_device_config_default, .side = RIGHT, .throttle = 1, .rotation = ROTATE_180, .motion = {.pin = TPS_RDY, .active_low = false}},
+    [2] = {.driver = &cirque_driver_spi_default, .comms_config = &cirque_config_spi_default, .device_config = &cirque_device_config_default, .side = LEFT, .throttle = 1, .rotation = ROTATE_270, .motion = {.pin = CIRQUE_RDY, .active_low = false}},
 };
 #endif
