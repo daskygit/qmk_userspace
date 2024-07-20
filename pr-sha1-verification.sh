664d08cb34e2931505b8dcc45a519125fc75ffe2
Dasky <32983009+daskygit@users.noreply.github.com>
2024-07-21 00:00:20 +0100

    unmac

--- pr-sha1-verification.sh
+++ pr-sha1-verification.sh
@@ -9,13 +9,20 @@ this_script="$(realpath "${BASH_SOURCE[0]}")"
 script_dir="$(realpath "$(dirname "$this_script")")"
 userspace_dir="$(realpath "$script_dir")"
 
+# Ensure we have access to the qmk venv if it exists
+venv_activate_path=$(find $userspace_dir -path '*/.direnv/python*/bin/*' -name activate | head -n1)
+if [[ ! -z "${venv_activate_path:-}" ]]; then
+    echo "Activating environment: $venv_activate_path"
+    source "$venv_activate_path"
+fi
+
 QMK_FIRMWARE_REPO=https://github.com/qmk/qmk_firmware.git
 reproducible_build_flags="-e COMMAND_ENABLE=no -e SKIP_VERSION=yes -e KEEP_INTERMEDIATES=yes -e USE_CCACHE=no"
 
 export RUNTIME=docker
 export NCPUS=$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null)
 
-errcho() { gecho "$@" 1>&2; }
+errcho() { echo "$@" 1>&2; }
 havecmd() { command command type "${1}" >/dev/null 2>&1 || return 1; }
 usage() {
     errcho "Usage: $(basename "$this_script") --pr <PR number> [--branch <branch>]"
@@ -73,23 +80,23 @@ cleanup() {
     cd "$userspace_dir"
     if [[ -z "${FIRST_INIT:-}" ]]; then
         if [[ -d "$build_dir" ]]; then
-            gecho "Entering shell so that extra investigation can occur..."
-            gecho "Current directory: $(pwd)"
-            gecho "Verification directory: $build_dir"
+            echo "Entering shell so that extra investigation can occur..."
+            echo "Current directory: $(pwd)"
+            echo "Verification directory: $build_dir"
             bash
         fi
     fi
-    if [[ -d "$build_dir" ]]; then
-        sleep 1
-        rm -rf "$build_dir"
-    fi
     if [[ -n "${USE_RAMDISK:-}" ]]; then
-        while [[ -n "$(mount | grep "/Volumes/RAM")" ]]; do
+        while [[ -n "$(mount | grep " $build_dir ")" ]]; do
             errcho "Waiting for $build_dir to unmount..."
             sleep 1
-            hdiutil detach /Volumes/RAM || true
+            sudo umount "$build_dir" || true
             sleep 1
         done
+    fi
+    if [[ -d "$build_dir" ]]; then
+        sleep 1
+        rm -rf --one-file-system "$build_dir"
     fi
 }
 trap cleanup EXIT INT TERM
@@ -98,10 +105,10 @@ build_targets() {
     pushd "$pr_dir" >/dev/null 2>&1
 
     # Auto-determined from modified files in diff, will traverse the children and pick out child boards too
-    pcmd git diff --name-only $TARGET_BRANCH | /opt/homebrew/opt/grep/libexec/gnubin/grep -P '^keyboards' | gsed -e 's@^keyboards/@@g' -e 's@/keymaps/.*$@@g' -e 's@/[^/]*$@@g' | while read kb; do
+    pcmd git diff --name-only $TARGET_BRANCH | grep -P '^keyboards' | sed -e 's@^keyboards/@@g' -e 's@/keymaps/.*$@@g' -e 's@/[^/]*$@@g' | while read kb; do
         find "keyboards/$kb" \( -name 'rules.mk' \) -and -not -path '*/keymaps/*' | while read rules; do
-            kb=$(dirname "$rules" | gsed -e 's@^keyboards/@@g')
-            gecho "${kb}:default"
+            kb=$(dirname "$rules" | sed -e 's@^keyboards/@@g')
+            echo "${kb}:default"
         done
     done | sort | uniq
 
@@ -110,7 +117,7 @@ build_targets() {
     #qmk find -km apa102
 
     # Explicit board names
-    #gecho takashiski/namecard2x4/rev1:default takashiski/namecard2x4/rev2:default
+    #echo takashiski/namecard2x4/rev1:default takashiski/namecard2x4/rev2:default
 
     # Random selection of 100 of all boards
     #qmk find | shuf | head -n100
@@ -120,13 +127,13 @@ build_targets() {
 
 build_one() {
     local target=$1
-    gecho $target
+    echo $target
     [[ -d .build ]] || mkdir .build
-    make ${target} $reproducible_build_flags 2>&1 | grep -v '⚠' >.build/$(gecho $target | gsed -e 's@/@_@g').log 2>&1
+    make ${target} $reproducible_build_flags 2>&1 | grep -v '⚠' >.build/$(echo $target | sed -e 's@/@_@g').log 2>&1
 }
 
 strip_calls() {
-    cat - | gsed \
+    cat - | sed \
         -e 's@call\s\+0x[0-9a-fA-F]\+@call 0x????@g' \
         -e 's@jmp\s\+0x[0-9a-fA-F]\+@jmp 0x????@g' \
         -e 's@lds\s\+r[0-9]\+,\s*0x[0-9a-fA-F]\+@lds r??, 0x????@g' \
@@ -137,11 +144,9 @@ main() {
     export FIRST_INIT=1
     cleanup
     unset FIRST_INIT
+    [[ -d "$build_dir" ]] || mkdir -p "$build_dir"
     if [[ -n "${USE_RAMDISK:-}" ]]; then
-        diskutil erasevolume APFS 'RAM' `hdiutil attach -nobrowse -nomount ram://67108864`
-        ln -s /Volumes/RAM/ "$build_dir"
-    else
-        [[ -d "$build_dir" ]] || mkdir -p "$build_dir"
+        sudo mount -t tmpfs tmpfs "$build_dir"
     fi
     cd "$build_dir"
 
@@ -169,7 +174,7 @@ main() {
     pcmd git merge --no-edit --quiet $TARGET_BRANCH
 
     # Generate the list of targets
-    targets=$(build_targets | sort | uniq | xargs gecho)
+    targets=$(build_targets | sort | uniq | xargs echo)
 
     if [[ -z "${targets:-}" ]]; then
         errcho "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
@@ -190,38 +195,38 @@ main() {
     { pcmd ${DOCKER_PREFIX:-} qmk mass-compile -c -j $NCPUS $reproducible_build_flags $targets || true; } | tee "$build_dir/mass_compile_pr.log"
     { ls -1 *.hex *.bin *.uf2 2>/dev/null || true; } | sort | xargs sha1sum >"$build_dir/sha1sums_pr.txt"
 
-    local differences=$( (diff -yW 200 --suppress-common-lines "$build_dir/sha1sums_base.txt" "$build_dir/sha1sums_pr.txt" || true) | awk '{print $2}' | gsed -e 's@\.\(hex\|bin\|uf2\)$@@g' | xargs gecho)
+    local differences=$( (diff -yW 200 --suppress-common-lines "$build_dir/sha1sums_base.txt" "$build_dir/sha1sums_pr.txt" || true) | awk '{print $2}' | sed -e 's@\.\(hex\|bin\|uf2\)$@@g' | xargs echo)
     for difference in $differences; do
         { find "$base_dir/.build/obj_$difference" "$pr_dir/.build/obj_$difference" -type f -name '*.i' 2>/dev/null || true; } | while read file; do
-            cat "$file" | gsed -e 's@^#.*@@g' -e 's@^\s*//.*@@g' -e '/^\s*$/d' -e 's@qmk_base@qmk_xxx@g' -e 's@qmk_pr@qmk_xxx@g' | clang-format >"$file.formatted"
+            cat "$file" | sed -e 's@^#.*@@g' -e 's@^\s*//.*@@g' -e '/^\s*$/d' -e 's@qmk_base@qmk_xxx@g' -e 's@qmk_pr@qmk_xxx@g' | clang-format >"$file.formatted"
         done
     done
 
-    gecho
-    gecho '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
-    gecho '@@ Listing builds that failed on the base revision:           @@'
-    gecho '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+    echo
+    echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+    echo '@@ Listing builds that failed on the base revision:           @@'
+    echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
     { cat "$build_dir/mass_compile_base.log" | grep ERR; } || true
 
-    gecho
-    gecho '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
-    gecho '@@ Listing builds that failed on the PR:                      @@'
-    gecho '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+    echo
+    echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+    echo '@@ Listing builds that failed on the PR:                      @@'
+    echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
     { cat "$build_dir/mass_compile_pr.log" | grep ERR; } || true
 
     # Work out the diff's between the two target builds
-    gecho
-    gecho '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
-    gecho '@@ Listing builds that differ between the base and target PR: @@'
-    gecho '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
-    gecho '@@ Begin @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
-    gecho
+    echo
+    echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+    echo '@@ Listing builds that differ between the base and target PR: @@'
+    echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+    echo '@@ Begin @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+    echo
     diff -yW 200 --suppress-common-lines "$build_dir/sha1sums_base.txt" "$build_dir/sha1sums_pr.txt" || true
-    gecho
-    gecho '@@ End @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
-    gecho '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+    echo
+    echo '@@ End @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+    echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 
-    elf_files=$( (diff "$build_dir/sha1sums_base.txt" "$build_dir/sha1sums_pr.txt" || true) | awk '/(hex|bin|uf2)/ {print $3}' | sort | uniq | gsed -e 's@\(hex\|bin\|uf2\)@elf@g' || true)
+    elf_files=$( (diff "$build_dir/sha1sums_base.txt" "$build_dir/sha1sums_pr.txt" || true) | awk '/(hex|bin|uf2)/ {print $3}' | sort | uniq | sed -e 's@\(hex\|bin\|uf2\)@elf@g' || true)
 
     objdump_params="-dS --no-addresses --no-show-raw-insn"
     for elf_file in $elf_files; do
