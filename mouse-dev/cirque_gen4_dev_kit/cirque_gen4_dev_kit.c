da09a8db7c50d7d0622ad53e1117649295c175a4
Dasky <32983009+daskygit@users.noreply.github.com>
2024-09-25 22:22:58 +0100

    kinetis bodge

--- mouse-dev/cirque_gen4_dev_kit/cirque_gen4_dev_kit.c
+++ mouse-dev/cirque_gen4_dev_kit/cirque_gen4_dev_kit.c
@@ -9,3 +9,21 @@ void board_init(void) {
     gpio_set_pin_output(D1);
     gpio_write_pin_high(D1); // Enable I2C pullups (IO14)
 }
+
+// fix pins because kinetis
+void i2c_init(void) {
+    static bool is_initialised = false;
+    if (!is_initialised) {
+        is_initialised = true;
+
+        // Try releasing special pins for a short time
+        palSetLineMode(I2C1_SCL_PIN, PAL_MODE_INPUT);
+        palSetLineMode(I2C1_SDA_PIN, PAL_MODE_INPUT);
+
+        chThdSleepMilliseconds(10);
+        palSetLineMode(I2C1_SCL_PIN, I2C1_SCL_PAL_MODE);
+        palSetLineMode(I2C1_SDA_PIN, I2C1_SDA_PAL_MODE);
+        PORTB->PCR[0] |= PORTx_PCRn_ODE;
+        PORTB->PCR[1] |= PORTx_PCRn_ODE;
+    }
+}
