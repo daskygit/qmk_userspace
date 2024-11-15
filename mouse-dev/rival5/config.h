d9e206dd45b93c456d3bfbf9ca8cc2b9417595e8
Dasky <32983009+daskygit@users.noreply.github.com>
2024-11-15 04:42:39 +0000

    i2c test

--- mouse-dev/rival5/config.h
+++ mouse-dev/rival5/config.h
@@ -1,5 +1,19 @@
+// Copyright 2024 Dasky (@daskygit)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #pragma once
 
 #define POINTING_DEVICE_MOTION_PIN C14
 #define POINTING_DEVICE_MOTION_PIN_ACTIVE_LOW
 #define DEBUG_MATRIX_SCAN_RATE
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SCL_PIN B6
+#define I2C1_SDA_PIN B7
+#define I2C1_SCL_PAL_MODE 1
+#define I2C1_SDA_PAL_MODE 1
+#define I2C1_TIMINGR_PRESC 0x00U
+#define I2C1_TIMINGR_SCLDEL 0x03U
+#define I2C1_TIMINGR_SDADEL 0x01U
+#define I2C1_TIMINGR_SCLH 0x03U
+#define I2C1_TIMINGR_SCLL 0x09U
--- mouse-dev/rival5/keymaps/default/keymap.c
+++ mouse-dev/rival5/keymaps/default/keymap.c
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "i2c_master.h"
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -22,13 +23,44 @@ uint32_t my_callback(uint32_t trigger_time, void *cb_arg) {
     return 0;
 }
 
+#define AW21036_ADDR (0x34 << 1)
+#define AW21036_TIMEOUT 10
+
 void keyboard_post_init_user(void) {
     // Customise these values to desired behaviour
     debug_enable = true;
-    debug_matrix = true;
+    // debug_matrix = true;
     // debug_keyboard=true;
     // debug_mouse=true;
     // defer_exec(30000, my_callback, NULL);
+
+    gpio_set_pin_output(A6);
+    gpio_write_pin_high(A6); // enable led output
+    wait_ms(100);
+
+    i2c_init();
+    uint8_t data = 0;
+    i2c_writeReg(AW21036_ADDR, 0x7F, &data, 1, AW21036_TIMEOUT);
+    wait_ms(5);
+    data = 0x07;
+    i2c_writeReg(AW21036_ADDR, 0x7C, &data, 1, AW21036_TIMEOUT);
+    i2c_writeReg(AW21036_ADDR, 0x70, (uint8_t *)0x80, 1, AW21036_TIMEOUT);
+    i2c_writeReg(AW21036_ADDR, 0x78, (uint8_t *)0x1C, 1, AW21036_TIMEOUT);
+    i2c_writeReg(AW21036_ADDR, 0x6E, (uint8_t *)0xFF, 1, AW21036_TIMEOUT);
+    i2c_writeReg(AW21036_ADDR, 0x90, (uint8_t *)0x37, 1, AW21036_TIMEOUT);
+    i2c_writeReg(AW21036_ADDR, 0x91, (uint8_t *)0x37, 1, AW21036_TIMEOUT);
+    i2c_writeReg(AW21036_ADDR, 0x92, (uint8_t *)0xFF, 1, AW21036_TIMEOUT);
+
+    for (uint8_t i = 0x46; i <= 0x6D; i++) {
+        i2c_writeReg(AW21036_ADDR, i, (uint8_t *)0xFF, 1, AW21036_TIMEOUT);
+    }
+
+    i2c_writeReg(AW21036_ADDR, 0x00, (uint8_t *)0x01, 1, AW21036_TIMEOUT);
+
+    for (uint8_t i = 0x01; i <= 0x24; i++) {
+        i2c_writeReg(AW21036_ADDR, i, (uint8_t *)0x5F, 1, AW21036_TIMEOUT);
+        i2c_writeReg16(AW21036_ADDR, 0x49, (uint8_t *)0x00, 1, AW21036_TIMEOUT);
+    }
 }
 
 void pointing_device_init_kb(void) {
@@ -40,3 +72,13 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = {ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
 };
 #endif
+
+void housekeeping_task_user(void) {
+    static uint32_t last_run = 0;
+
+    if (timer_elapsed32(last_run) < 1000) return;
+    last_run = timer_read32();
+    // uint8_t      data   = 0;
+    // i2c_status_t status = i2c_writeReg(AW21036_ADDR, 0x7F, &data, 1, AW21036_TIMEOUT);
+    // printf("i2c: %d\n", status);
+}
